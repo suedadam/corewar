b83af35057d8db1a72f16115432d405bdef81e78
David Rosa Tamsen <rosatamsen@gmail.com>
2018-03-07 00:29:12 -0800

    champion: assembled our_champ, example champs, and added test.sh script

---
+++ vm/example/champs/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+../asm our_champ.s
+
+../corewar our_champ.cor Gagnant.cor
+../corewar our_champ.cor Octobre_Rouge_V4.2.cor
+../corewar our_champ.cor ex.cor
+../corewar our_champ.cor slider2.cor
+../corewar our_champ.cor examples/zork.cor
+../corewar our_champ.cor examples/turtle.cor
+../corewar our_champ.cor examples/bigzork.cor
+../corewar our_champ.cor examples/bee_gees.cor
+../corewar our_champ.cor examples/fluttershy.cor
+../corewar our_champ.cor mortel.cor
+../corewar our_champ.cor jumper.cor
+../corewar our_champ.cor maxidef.cor
+../corewar our_champ.cor examples/helltrain.cor
